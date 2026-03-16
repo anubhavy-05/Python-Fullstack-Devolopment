65a59b55ea66a457c7303b99d39633f263413159
Anubhav yadav <ay8400673@gmail.com>
2026-03-17 00:34:49 +0530

    Refactor variable assignment and type checking examples for clarity and consistency

--- INTRODUCTION/Main.py
+++ INTRODUCTION/Main.py
@@ -35,11 +35,19 @@ print("Is x less than y?", x<y)
 print("Is x equal to y?", x==y)
 print("Is x not equal to y?", x!=y)   
 
+#create a variable by the name of my_var and assign it an integer value. Then, print the data type of my_var. Next, reassign my_var with a string value and print the updated data type.
 
-my_var=50
-print(type(my_var))
-my_var="Hello"
-print(type(my_var))
+my_var=50  # Assign an integer value to my_var.
+print(type(my_var))  # Print the current data type of my_var (int).
+my_var="Hello"  # Reassign my_var with a string value.
+print(type(my_var))  # Print the updated data type of my_var (str).
 
 
-    
+# Create two variables, num1 and num2, and assign them the same integer value. Then, print the memory addresses of both variables and check if they refer to the same object in memory.
+
+num1=500
+num2=500
+print(memory  address of num1:", id(num1))  # Print the memory address of num1.
+print("Memory address of num2:", id(num2))  # Print the memory address of num2.
+print("Are num1 and num2 the same object?", num1 is num2)  # Check if num1 and num2 refer to the same object in memory.
+print(id(num1) == id(num2))  # Check if the memory addresses of num1 and num2 are the same. 
