dbcdc4cb469c41938f5c2335775da3cb8697abad
Anubhav yadav <ay8400673@gmail.com>
2026-03-16 23:51:00 +0530

    Add initial Python program for variable assignment and type checking

---
+++ INTRODUCTION/Main.py
@@ -0,0 +1,12 @@
+# This is a simple Python program that demonstrates variable assignment and type checking.
+
+name="Anubhav"
+age=20
+price=100.50
+print("My name is", name)
+print("My age is", age)
+print("The price is", price)
+print(type(name))
+print(type(age))
+print(type(price))
+
