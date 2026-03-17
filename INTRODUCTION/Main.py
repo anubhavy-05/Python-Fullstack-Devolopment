8f91c66f9aaa847d21d347c575964b5d32aedddc
Anubhav yadav <ay8400673@gmail.com>
2026-03-18 00:28:58 +0530

    Add user input functionality for age and basic arithmetic operations in Main.py

--- INTRODUCTION/Main.py
+++ INTRODUCTION/Main.py
@@ -51,3 +51,19 @@ print("Memory address of num1:", id(num1))  # Print the memory address of num1.
 print("Memory address of num2:", id(num2))  # Print the memory address of num2.
 print("Are num1 and num2 the same object?", num1 is num2)  # Check if num1 and num2 refer to the same object in memory.
 print(id(num1) == id(num2))  # Check if the memory addresses of num1 and num2 are the same. 
+
+
+x=input("enter your age:")  # Prompt the user to enter their age and store it in variable x.
+print("Your age is", x)  # Print the age entered by the user.
+
+#WAP to take input 2 nubers from user and print their sum, difference, product and quotient.
+num1=int(input("Enter the first number: "))  # Take the first number as input from the user and convert it to an integer.
+num2=int(input("Enter the second number: "))  # Take the second number as input from the user and convert it to an integer.
+sum=num1+num2  # Calculate the sum of num1 and num2.
+difference=num1-num2  # Calculate the difference of num1 and num2.
+product=num1*num2  # Calculate the product of num1 and num2.
+quotient=num1/num2  # Calculate the quotient of num1 and num2.
+print("Sum:", sum)
+print("Difference:", difference)
+print("Product:", product)
+print("Quotient:", quotient)    
