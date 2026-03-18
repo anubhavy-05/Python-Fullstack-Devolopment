201d8b5d419779e8e07ac5c1e3bf8a0870bce592
Anubhav yadav <ay8400673@gmail.com>
2026-03-19 00:25:43 +0530

    Replace arithmetic operations with positive/negative number check in tempCodeRunnerFile.py

--- INTRODUCTION/tempCodeRunnerFile.py
+++ INTRODUCTION/tempCodeRunnerFile.py
@@ -1,10 +1,6 @@
-num1=int(input("Enter the first number: "))  # Take the first number as input from the user and convert it to an integer.
-num2=int(input("Enter the second number: "))  # Take the second number as input from the user and convert it to an integer.
-sum=num1+num2  # Calculate the sum of num1 and num2.
-difference=num1-num2  # Calculate the difference of num1 and num2.
-product=num1*num2  # Calculate the product of num1 and num2.
-quotient=num1/num2  # Calculate the quotient of num1 and num2.
-print("Sum:", sum)
-print("Difference:", difference)
-print("Product:", product)
-print("Quotient:", quotient)   
+
+x=int(input("enter the number:"))
+if x>=0:
+    print("the number is positive")
+if x<0:
+    print("the number is non-positive")
