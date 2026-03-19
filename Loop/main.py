3e7c9aaba2d3773456329c953aad49c233607f02
Anubhav yadav <ay8400673@gmail.com>
2026-03-19 22:52:30 +0530

    Add initial implementations for counting and reverse printing using while loops

---
+++ Loop/main.py
@@ -0,0 +1,15 @@
+i=1 
+while i<=5:
+    print(i)
+    i+=1    
+#WAP to print hello five times using while loop.
+i=1 
+while i<=5:
+    print("Hello")
+    i+=1    
+
+#WAP TO PRINT 100 TO 1 IN REVERSE ORDER USING WHILE LOOP.
+i=100       
+while i>=1:
+    print(i)
+    i-=1      
