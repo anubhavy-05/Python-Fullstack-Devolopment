5153f75cec6db1d4b7345e6d93655cf26e6038ee
Anubhav yadav <ay8400673@gmail.com>
2026-03-20 19:37:43 +0530

    Add user input functionality for multiplication table and squares of first 10 natural numbers

--- Loop/main.py
+++ Loop/main.py
@@ -13,3 +13,17 @@ i=100
 while i>=1:
     print(i)
     i-=1      
+
+#WAP to take a number from user and print its multiplication table using while loop.
+X=int(input("enter the number:"))
+i=1
+while i<=10:
+    print(X*i)
+    i+=1
+
+#WAP to print the squares of first 10 natural numbers using while loop.
+i=1
+while i<=10:
+    print(i**2)
+    i+=1
+    
