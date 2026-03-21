58548005072c58bc4f39a6c6ea00c3b88e331514
Anubhav yadav <ay8400673@gmail.com>
2026-03-21 09:57:34 +0530

    Add additional examples for number swapping and implement checks for divisibility, three-digit numbers, and digit count

--- Practice.py
+++ Practice.py
@@ -23,3 +23,36 @@ print("The value of a is:", a)
 print("The value of b is:", b)
 
                  #OR
+a=a+b
+b=a-b
+a=a-b
+print("The value of a is:", a)
+print("The value of b is:", b)
+
+                  #OR
+a=5
+b=10
+a,b=b,a
+print("The value of a is:", a)
+print("The value of b is:", b)
+
+#WAP to check if a number is divisible by 5. Take the number as input from the user.
+
+x=int(input("Enter a number:"))
+if x%5==0:
+    print("The number is divisible by 5.")
+
+
+
+#WAP to check wheather a nuber is three digit or not. Take the number as input from the user.
+
+number=int(input("Enter a number:"))
+if 100<=number<=999:
+    print("The number is a three-digit number.")
+
+#WAP to check how many digits are there in a number. Take the number as input from the user.
+number=int(input("Enter a number:"))
+digit_count=len(str(number))
+print("The number of digits in the number is:", digit_count)
+
+
