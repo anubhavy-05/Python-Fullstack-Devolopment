839fb3f971bbb9b8cbe1380db6d8a8838ce9d83e
Anubhav yadav <ay8400673@gmail.com>
2026-03-21 09:56:11 +0530

    Add initial implementation for rectangle area calculation, last digit removal, and number swapping

---
+++ Practice.py
@@ -0,0 +1,25 @@
+
+#WAP to calculate the area of a rectangle. Take the length and breadth as input from the user.
+
+Length=int(input("Enter "))
+breath=int(input("Enter "))
+area=Length*breath
+print("The area of the rectangle is:", area)
+
+
+
+#WAP TO REMOVE THE LAST DIGIT FROM A NUMBER. TAKE THE NUMBER AS INPUT FROM THE USER.
+
+number=int(input("Enter a number:"))
+result=number//10
+print("The number after removing the last digit is:", result)
+
+#WAP to swap two numbers without using a third variable. Take the two numbers as input from the user.
+
+a=int(input("Enter the first number:")) 
+b=int(input("Enter the second number:"))
+a,b=b,a
+print("The value of a is:", a)
+print("The value of b is:", b)
+
+                 #OR
