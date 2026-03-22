66715f96133af9b0f6e61c40e35d32e4bc4c7395
Anubhav yadav <ay8400673@gmail.com>
2026-03-23 01:41:58 +0530

    Add string manipulation examples to main.py

---
+++ String/main.py
@@ -0,0 +1,18 @@
+S1="mysirg"
+print(S1)
+print(S1[0]) # To access the first character of the string.
+print(S1[2]) # To access the third character of the string. 
+print(S1[-1]) # To access the last character of the string.
+print(S1[0:3]) # To get a substring of the first three characters.
+print(S1[2:5]) # To get a substring of the third to fifth characters.
+print(min(S1)) # To find the minimum character in the string.
+print(max(S1)) # To find the maximum character in the string.   
+
+
+sorted_S1=sorted(S1) # To sort the characters of the string in ascending order.
+print("The sorted characters of the string are:", sorted_S1)   
+
+
+
+
+
