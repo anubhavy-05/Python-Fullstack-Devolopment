c240eb8bf6f8c4001c6e8733c72a7f3a3ad41b0b
Anubhav yadav <ay8400673@gmail.com>
2026-03-23 01:41:49 +0530

    Replace password validation logic with prime number listing functionality

--- Loop/tempCodeRunnerFile.py
+++ Loop/tempCodeRunnerFile.py
@@ -1,8 +1,7 @@
-pass="python123"    
-while True:
-    password = input("enter the password:")
-    if password == pass:
-        print("correct password, welcome to the system")
-        break
-    else:
-        print("wrong password, try again")
+for num in range(1, 101):
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                break
+        else:
+            print(num)
