8b307da725f4d06d297e07aeb215a7f1966a7b92
Anubhav yadav <ay8400673@gmail.com>
2026-03-24 22:51:21 +0530

    Add concatenation, repetition, and comparison operations for strings in main.py

--- String/main.py
+++ String/main.py
@@ -12,6 +12,25 @@ print(max(S1)) # To find the maximum character in the string.
 sorted_S1=sorted(S1) # To sort the characters of the string in ascending order.
 print("The sorted characters of the string are:", sorted_S1)   
 
+               
+              #concatination and repetition of string
+
+S1="sir"
+S2="mysir"
+S3=S1+S2 # Concatenation of S1 and S2.
+print("The concatenated string is:", S3)
+S4=S1*3 # Repetition of S1 three times.
+print("The repeated string is:", S4)
+
+
+                 #comperision operators
+
+S1="abc"
+S2="xyz"    
+print(S1==S2) # To check if S1 and S2 are equal.
+print(S1!=S2) # To check if S1 and S2 are not equal
+print(S1>S2) # To check if S1 is greater than S2 (lexicographically).
+print(S1<S2) # To check if S1 is less than S2 (lexicographically).
 
 
 
