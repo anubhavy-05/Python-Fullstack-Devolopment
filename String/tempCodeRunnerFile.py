fde4e9b2ff0d03b3f599faae572eed4a2a3998eb
Anubhav yadav <ay8400673@gmail.com>
2026-03-24 23:47:52 +0530

    Add initial implementation of string splitting and joining in tempCodeRunnerFile.py

---
+++ String/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+sentence = "Hello, how are you?"
+words = sentence.split() # To split the sentence into a list of words.
+print(words)
+delimiter = "-"
+joined_sentence = delimiter.join(words) # To join the list of words into a single string using the delimiter.
+print(joined_sentence) 
