215717b8f01e7f62e17a481cd5775b74ddce30fd
Anubhav yadav <ay8400673@gmail.com>
2026-03-27 23:29:38 +0530

    Refactor comments in main.py; improve clarity and formatting for set examples

--- Set/main.py
+++ Set/main.py
@@ -1,4 +1,10 @@
-# set 
+                        # set 
+
+
+
 s1 = {1, 2, 3, 4, 5}
 print(s1) # Output: {1, 2, 3, 4, 5} (sets are unordered collections of unique elements)
 print(type(s1)) # Output: <class 'set'> (s1  is a set)
+
+s2 = {}
+print(type(s2)) # Output: <class 'dict'> (s2 is an empty dictionary, not a set), to create an empty set, you should use s2 = set()
